7ccf4172090d654241c6f2aa9845c5607f851c8b
Febielyne Nicolas <Febielyne.Nicolas@students.ittralee.ie>
2020-02-11 10:06:58 +0000

    Initial Commit of unity Project Signed-off-by: Febielyne Nicolas <Febielyne.Nicolas@students.ittralee.ie>

---
+++ Touch Control Assignment/Assets/Controlable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlable : MonoBehaviour
+{
+    Renderer my_renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    internal void move_up()
+    {
+        transform.position += Vector3.up;
+
+    }
+
+    internal void go_red()
+    {
+        my_renderer.material.color = Color.red;
+    }
+
+    internal void go_blue()
+    {
+        my_renderer.material.color = Color.blue;
+    }
+
+    internal void go_yellow()
+    {
+        my_renderer.material.color = Color.yellow;
+    }
+    internal void go_white()
+    {
+        my_renderer.material.color = Color.white;
+    }
+
+    internal void select()
+    {
+        go_red();
+    }
+
+    internal void deselect()
+    {
+        go_white();
+    }
+}
---
+++ Touch Control Assignment/Assets/TouchControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchControl : MonoBehaviour
+{
+   
+    private float[] timeTouchBegan;
+    private bool[] touchDidMove;
+    private float tapTimeThreshold = .5f;
+    Controlable currently_selected_item;
+
+    Camera my_camera = new Camera();
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_camera = Camera.main;
+         timeTouchBegan = new float[10];
+        touchDidMove = new bool[10];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+
+        foreach (Touch touch in Input.touches)
+        {
+            int fingerIndex = touch.fingerId;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Finger #" + fingerIndex.ToString() + " entered!");
+                timeTouchBegan[fingerIndex] = Time.time;
+                touchDidMove[fingerIndex] = false;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Debug.Log("Finger #" + fingerIndex.ToString() + " moved!");
+                touchDidMove[fingerIndex] = true;
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                float tapTime = Time.time - timeTouchBegan[fingerIndex];
+                Debug.Log("Finger #" + fingerIndex.ToString() + " left. Tap time: " + tapTime.ToString());
+                if (tapTime <= tapTimeThreshold && touchDidMove[fingerIndex] == false)
+                {
+                    // Select the object
+                    Ray my_ray = my_camera.ScreenPointToRay(touch.position);
+                    Debug.DrawRay(my_ray.origin, 20 * my_ray.direction);
+
+                    RaycastHit info_on_hit;
+                    if (Physics.Raycast(my_ray, out info_on_hit))
+                    {
+                        Controlable my_obj = info_on_hit.transform.GetComponent<Controlable>();
+                        if(my_obj)
+                        {
+                            if (currently_selected_item)
+                            {
+                                currently_selected_item.deselect();
+                            }
+                            my_obj.select();
+                            currently_selected_item = my_obj;
+
+                        }
+
+                    }
+                    else
+                    {
+                        if (currently_selected_item)
+                        {
+                            currently_selected_item.deselect();
+                            currently_selected_item = null;
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
